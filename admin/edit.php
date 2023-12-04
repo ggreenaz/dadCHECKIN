e22dd13441e3a73009da3c3de7cc6f093e165383
ggreenaz <ggreenaz@gmail.com>
2023-12-04 12:22:21 -0500

    Update edit.php
    added <link rel="stylesheet" type="text/css" href="theme.php"> to apply multiple themes.

--- admin/edit.php
+++ admin/edit.php
@@ -184,7 +184,8 @@ $conn->close();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Visitor Management System</title>
-<link rel="stylesheet" type="text/css" href="theme.php">    <style>
+<link rel="stylesheet" type="text/css" href="theme.php"><body>  
+<style>
         /* Center-align the edit tables */
         table.edit-table {
             width: 40%;
